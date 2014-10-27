c4455359891468347b5df8101849748702002354
Kaustubh <kausvartak@gmail.com>
2014-10-27 00:17:52 -0400

    k mean working

---
+++ src/edu/buffalo/cse/clustering/KMeans.java
@@ -0,0 +1,90 @@
+package edu.buffalo.cse.clustering;
+
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.List;
+import java.util.Random;
+
+
+
+public class KMeans {
+	public static void main(String args[])
+	{
+		List<String> data = null;
+		int k = 5;
+		try {
+			data = Files.readAllLines(Paths.get("data/cho.txt"));
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			System.out.println("Data file not found");
+			e.printStackTrace();
+		}
+		int m = data.size();
+		int n = data.get(0).split("\t").length;
+		float[][] genes = new float[m][n+1];
+		
+		for (int i = 0; i < m; i++)
+		{
+			String[] tuple = data.get(i).split("\t");
+			for (int j = 0; j < n; j++)
+			{
+				genes[i][j] = Float.parseFloat(tuple[j]);
+			}
+		}
+		
+		float[][] means = new float[k][n-2];
+		
+		for (int i = 0; i < k; i++)
+			for (int j = 0; j < n-2; j++)
+				means[i][j] = (new Random()).nextFloat()*(9.0f) - 4.5f;
+		
+		boolean converged = false;
+		while (!converged)
+		{
+			converged = true;
+			for (int i = 0; i < m; i++)
+			{
+				int current_median = (int)genes[i][n];
+				float mind = 0f;
+				for (int j = 0; j < n-2; j++)
+					mind += Math.pow(genes[i][j+2] - means[current_median][j], 2);
+				
+				for (int h = 0; h < k; h++)
+				{
+					float current_mind = 0;
+					for (int j = 0; j < n-2; j++)
+						current_mind += Math.pow(genes[i][j+2] - means[h][j], 2);	
+					if (current_mind < mind)
+					{
+						converged = false;
+						mind = current_mind;
+						genes[i][n] = h;
+					}
+				}
+			}
+			for (int h = 0; h < k; h++)
+			{
+				for (int j = 0; j < n-2; j++)
+				{
+					int count = 0;
+					float sum = 0f;
+					for (int i = 0; i < m; i++)
+					{
+						if (h == (int)genes[i][n])
+						{
+							count++;
+							sum += genes[i][j+2];
+						}
+					}
+					if (count != 0)
+						means[h][j] = sum/count;
+				}
+			}
+		}
+		for (int i = 0; i < m; i++)
+			System.out.println(genes[i][0] + " " + genes[i][n] + " " + genes[i][1]);
+		
+	}
+
+}
